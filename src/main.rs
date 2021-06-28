bbcbc86195a94a1cf2d8f7b86faac41ce4098d63
Michael Walker <michaelwalker@quickenloans.com>
2021-06-28 17:00:11 -0400

    working on the changes to create the window

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,60 @@
-use bindings::Windows::{
-    Win32::System::Threading::{CreateEventW, SetEvent, WaitForSingleObject},
-    Win32::System::WindowsProgramming::CloseHandle,
-    Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK, MB_ICONEXCLAMATION},
+use bindings::{
+    Windows::{
+        Win32::{
+            Foundation::*,
+            UI::{
+                Animation::*,
+                WindowsAndMessaging::*,
+            }
+        }
+    }
 };
 
-fn main() -> windows::Result<()> {
-    unsafe {
-        let event = CreateEventW(std::ptr::null_mut(), true, false, None);
-        SetEvent(event).ok()?;
-        WaitForSingleObject(event, 0);
-        CloseHandle(event).ok()?;
+use windows::*;
 
-        MessageBoxA(None, "THERE CAN BE ONLY ONE!!!", "MY FIRST RUST-BASED WINDOWS PROGRAM", MB_OK | MB_ICONEXCLAMATION);
+fn main() -> Result<()> {
+    initialize_sta()?;
+    let mut window = Window::new()?;
+    window.run()
+}
+
+struct Window {
+    handle: HWND,
+    ps: PAINTSTRUCT,
+    hdc: HDC,
+}
+
+impl Window {
+    
+
+    fn message_handler(&mut self, message u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+        unsafe {
+            match message {
+                WM_CREATE => {
+                    // do initialization stuff here
+
+                    // return success
+                    LRESULT(0)
+                }
+                WM_PAINT {
+                    // simply validate the window
+                    hdc = BeginPaint(self.handle, &self.ps);
+                    // you would do all your painting here
+                    EndPaint(self.handle, &self.ps)
+
+                    // return success
+                    LRESULT(0)
+                }
+                WM_DESTROY => {
+                    // kill the application, this sends a WM_QUIT message
+                    PostQuitMessage(0);
+
+                    // return success
+                    LRESULT(0)
+                }
+                // process any message that we didn't take care of
+                _ => DefWindowProcA(self.handle, message, wparam, lparam),
+            }
+        }
     }
-
-    Ok(())
 }
