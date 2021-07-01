f857409568a706104d8f630560be7a0274fe887d
Michael Walker <michaelwalker@quickenloans.com>
2021-07-01 16:49:42 -0400

    removing unnecessary items, and adding in the DirectDraw Libraries

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,21 +1,11 @@
 fn main() {
     windows::build!(
         Windows::{
-            Foundation::Numerics::Matrix3x2,
             Win32::{
-                Foundation::{DXGI_STATUS_OCCLUDED, HINSTANCE, HWND, LPARAM, LRESULT, PWSTR, WPARAM},
+                Foundation::{HINSTANCE, HWND, LPARAM, LRESULT, PWSTR, WPARAM},
                 Graphics::{
-                    Direct2D::{
-                        CLSID_D2D1Shadow, D2D1CreateFactory, ID2D1Bitmap1, ID2D1Device,
-                        ID2D1DeviceContext, ID2D1Effect, ID2D1Factory1, ID2D1SolidColorBrush,
-                    },
-                    Direct3D11::{D3D11CreateDevice, ID3D11Device, D3D11_SDK_VERSION},
-                    Dxgi::{
-                        CreateDXGIFactory1, IDXGIAdapter, IDXGIDevice, IDXGIFactory2, IDXGISwapChain1,
-                        DXGI_ERROR_UNSUPPORTED, DXGI_FORMAT, DXGI_PRESENT_TEST,
-                        DXGI_USAGE_RENDER_TARGET_OUTPUT,
-                    },
-                    Gdi::{BeginPaint, EndPaint, GetStockObject, CreateSolidBrush, PAINTSTRUCT, RGBTRIPLE, HDC, HBRUSH},
+                    DirectDraw::*,
+                    Gdi::{BeginPaint, EndPaint, GetStockObject, CreateSolidBrush, PAINTSTRUCT, HDC, HBRUSH},
                 },
                 System::{
                     LibraryLoader::GetModuleHandleW,
@@ -23,18 +13,13 @@ fn main() {
                     SystemInformation::GetLocalTime,
                 },
                 UI::{
-                    Animation::{
-                        IUIAnimationManager, IUIAnimationTransitionLibrary, IUIAnimationVariable,
-                        UIAnimationManager, UIAnimationTransitionLibrary,
-                    },
                     WindowsAndMessaging::{
-                        CreateWindowExW, DefWindowProcW, DispatchMessageW, GetMessageW, GetWindowLongW,
+                        CreateWindowExW, DefWindowProcW, DispatchMessageW,
                         GetWindowLongPtrW, LoadCursorW, PeekMessageW, PostQuitMessage, RegisterClassExW,
                         SetWindowLongW, SetWindowLongPtrW, TranslateMessage, LoadIconW, SendMessageW,
-                        CREATESTRUCTW, CW_USEDEFAULT,
-                        IDC_HAND, IDC_ARROW, MSG, SIZE_MINIMIZED, WINDOW_LONG_PTR_INDEX, WM_ACTIVATE, WM_DESTROY,
+                        CREATESTRUCTW, CW_USEDEFAULT, IDC_ARROW, MSG, WM_DESTROY,
                         WM_CREATE, IDI_APPLICATION, WM_CLOSE, VK_ESCAPE, VK_SPACE, VK_RETURN,
-                        WM_DISPLAYCHANGE, WM_NCCREATE, WM_PAINT, WM_QUIT, WM_SIZE, WM_USER, WNDCLASSEXW,
+                        WM_PAINT, WM_QUIT, WM_SIZE, WM_USER, WNDCLASSEXW,
                     },
                     KeyboardAndMouseInput::GetAsyncKeyState,
                 },
